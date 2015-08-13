27b8ec96abcf1ff6c969ff20ae892e153de09bec
iamwave007 <iamwave007@hotmail.com>
2015-08-13 02:56:00 -0700

    discovering weights to training time

--- xx.py
+++ xx.py
@@ -1,9 +1,8 @@
 from random import choice 
 from numpy import array, dot, random 
 from pylab import plot, ylim
-# plot weight change over n and see the curve
-# dot(w, x): the third value is random as well... ok. what if it's not?
-# how to save the trained ability somewhere for later use?
+
+# dot(w, x): the third value is random as well...what if it's not? # no matter
 # weird data:
 # training_data = [ 
 #   (array([0,0,1]), 1), 
@@ -12,8 +11,9 @@ from pylab import plot, ylim
 #   (array([1,1,1]), 0), 
 # ]
 
-# ? dummy: if not for it, there might be cases where learning is not needed
-#      and this won't be a good example then...
+# how to save the trained ability somewhere for later use?
+# what pattern does the vector w follow?
+
 training_data = [ 
   (array([0,0,1]), 0), 
   (array([0,1,1]), 1), 
@@ -26,6 +26,7 @@ w = random.rand(3)
 print w, " is w is here"
 unit_step = lambda x: 0 if x < 0 else 1 
 errors = []
+weights_2=[]
 eta = 0.2   #0.2
 n = 100 
 
@@ -36,14 +37,19 @@ for i in range(n):
   errors.append(error) 
   w += eta * error * x
   print w[2],error
-  # print w  # this is clumsy. changing each is better?
-
-# for x, _ in training_data: 
-#   result = dot(x, w)
-#   print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
+  weights_2.append(w[2])
 
 
+for x, _ in training_data: 
+  result = dot(x, w)
+  print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
+
+
+# plot weight change over n and see the curve
 
 from pylab import *
-plot(range(n),errors)
+# plot(range(n),errors)
+plot(range(n),weights_2)
 show()
+
+
