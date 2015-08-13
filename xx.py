3001067dc3ada1720f198400f4dc7c6c66665843
iamwave007 <iamwave007@hotmail.com>
2015-08-13 06:35:23 -0700

    one more example, one more plot

--- xx.py
+++ xx.py
@@ -2,15 +2,6 @@ from random import choice
 from numpy import array, dot, random 
 from pylab import plot, ylim
 
-# dot(w, x): the third value is random as well...what if it's not? # no matter
-# weird data:
-# training_data = [ 
-#   (array([0,0,1]), 1), 
-#   (array([0,1,1]), 0), 
-#   (array([1,0,1]), 0), 
-#   (array([1,1,1]), 0), 
-# ]
-
 # how to save the trained ability somewhere for later use?
 # what pattern does the vector w follow?
 
@@ -22,16 +13,15 @@ training_data = [
 ]
 
 w = random.rand(3)
-# w=[0,0,0]
 print w, " is w is here"
 unit_step = lambda x: 0 if x < 0 else 1 
 errors = []
 weights_2=[]
-eta = 0.2   #0.2
+eta = 0.2   
 n = 100 
 
 for i in range(n):
-  x, expected = choice(training_data) 
+  x, expected = choice(training_data)
   result = dot(w, x) 
   error = expected - unit_step(result)
   errors.append(error) 
@@ -45,11 +35,7 @@ for x, _ in training_data:
   print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
 
 
-# plot weight change over n and see the curve
-
 from pylab import *
 # plot(range(n),errors)
 plot(range(n),weights_2)
 show()
-
-
--- xxx.py
+++ xxx.py
@@ -1,6 +1,27 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from numpy import linspace, sin, cos
+from pylab import figure, show
+from mpl_toolkits.mplot3d import Axes3D
 
-x = np.arange(0, 5, 0.1);
-y = np.sin(x)
-plt.plot(x, y)
+# generating some data
+x = linspace(-10,10,100);
+y = sin(x);
+z = cos(x);
+
+fig = figure()
+ 
+ax = Axes3D(fig)
+
+# plotting the stems
+for i in range(len(x)):
+  ax.plot([x[i], x[i]], [y[i], y[i]], [0, z[i]], 
+          '--', linewidth=2, color='b', alpha=.5)
+
+# plotting a circle on the top of each stem
+ax.plot(x, y, z, 'o', markersize=8, 
+        markerfacecolor='none', color='b',label='ib')
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+show()
